bba9a14838483fdfe56fec60658b263305226996
HomerJSimpson1 <vuke93@gmx.com>
2016-07-08 17:57:15 -0400

    Latest updates enronRestore.js

--- Pymon/Final/enronRestore.js
+++ Pymon/Final/enronRestore.js
@@ -797,4 +797,254 @@ Type "it" for more
 
 
 
+
 // See final7.py file for the python code using pymongo to eliminate orphaned records
+
+// The question as laid out in the Final Exam:
+// 
+// Download Handouts:
+// final7__f7_m101_52e000fde2d423744501d031.zip
+// You have been tasked to cleanup a photo-sharing database. The database consists of two collections, albums, and images. Every image is supposed to be in an album, but there are orphan images that appear in no album. Here are some example documents (not from the collections you will be downloading).
+// > db.albums.findOne()
+// {
+//     "_id" : 67
+//     "images" : [
+//         4745,
+//         7651,
+//         15247,
+//         17517,
+//         17853,
+//         20529,
+//         22640,
+//         27299,
+//         27997,
+//         32930,
+//         35591,
+//         48969,
+//         52901,
+//         57320,
+//         96342,
+//         99705
+//     ]
+// }
+
+// > db.images.findOne()
+// { "_id" : 99705, "height" : 480, "width" : 640, "tags" : [ "dogs", "kittens", "work" ] }
+// From the above, you can conclude that the image with _id = 99705 is in album 67. It is not an orphan.
+// Your task is to write a program to remove every image from the images collection that appears in no album. Or put another way, if an image does not appear in at least one album, it's an orphan and should be removed from the images collection.
+// Download final7.zip from Download Handout link and use mongoimport to import the collections in albums.json and images.json.
+// When you are done removing the orphan images from the collection, there should be 89,737 documents in the images collection. To prove you did it correctly, what are the total number of images with the tag 'kittens" after the removal of orphans? As as a sanity check, there are 49,932 images that are tagged 'kittens' before you remove the images.
+// Hint: you might consider creating an index or two or your program will take a long time to run.
+// Choose the best answer:
+// 49,932
+// 47,678
+// 38,934
+// 45,911
+// 44,822
+// Submit
+// Your submission has been saved, and will be graded when the problem closes.
+
+// My answer was 44,822
+
+
+
+
+
+
+// Final Question #8:
+// The question as laid out on the final exam:
+
+// Suppose you have a three node replica set. Node 1 is the primary. Node 2 is a secondary, and node 3 is a secondary running with a delay of two hours. All writes to the database are issued with w=majority and j=1 (by which we mean that the getLastError call has those values set).
+
+// A write operation (which could be insert or update) is initiated from your application using the pymongo driver at time t=0. At time t=5 seconds, the primary (Node 1), goes down for an hour and Node 2 is elected primary. Note that your write operation has not yet returned at the time of the failure. Note also that although you have not received a response from the write, it has been processed and written by Node 1 before the failure. Node 3, since it has a slave delay option set, is lagging.
+
+// Will there be a rollback of data on Node 1 when it comes back up? Choose the best answer.
+
+// Choose the best answer:
+// Yes, always
+// No, never
+// Maybe, it depends on whether Node 3 has processed the write.
+// Maybe, it depends on whether Node 2 has processed the write.
+// Submit
+// Your submission has been saved, and will be graded when the problem closes.
+
+
+
+// My answer: Maybe, it depends on whether Node 2 has processed the write
+
+
+
+
+
+
+
+// Final Question #9:
+// The question as laid out on the final exam:
+
+// Imagine an electronic medical record database designed to hold the medical records of every individual in the United States. Because each person has more than 16MB of medical history and records, it's not feasible to have a single document for every patient. Instead, there is a patient collection that contains basic information on each person and maps the person to a patient_id, and a record collection that contains one document for each test or procedure. One patient may have dozens or even hundreds of documents in the record collection.
+
+// We need to decide on a shard key to shard the record collection. What's the best shard key for the record collection, provided that we are willing to run inefficient scatter-gather operations to do infrequent research and run studies on various diseases and cohorts? That is, think mostly about the operational aspects of such a system. And by operational, we mean, think about what the most common operations that this systems needs to perform day in and day out.
+
+// Choose the best answer:
+// patient_id
+// _id
+// Primary care physician (your principal doctor that handles everyday problems)
+// Date and time when medical record was created
+// Patient first name
+// Patient last name
+// Submit
+// Your submission has been saved, and will be graded when the problem closes.
+
+// My answer: patient_id
+
+
+
+
+// Final Question #10: 
+// The question as laid out on the final exam:
+
+// Understanding the output of explain
+
+// We perform the following query on the enron dataset:
+
+// var exp = db.messages.explain('executionStats')
+
+// exp.find( { 'headers.Date' : { '$gt' : new Date(2001,3,1) } }, { 'headers.From' : 1, '_id' : 0 } ).sort( { 'headers.From' : 1 } )
+// and get the following explain output.
+
+// {
+//   "queryPlanner" : {
+//     "plannerVersion" : 1,
+//     "namespace" : "enron.messages",
+//     "indexFilterSet" : false,
+//     "parsedQuery" : {
+//       "headers.Date" : {
+//         "$gt" : ISODate("2001-04-01T05:00:00Z")
+//       }
+//     },
+//     "winningPlan" : {
+//       "stage" : "PROJECTION",
+//       "transformBy" : {
+//         "headers.From" : 1,
+//         "_id" : 0
+//       },
+//       "inputStage" : {
+//         "stage" : "FETCH",
+//         "filter" : {
+//           "headers.Date" : {
+//             "$gt" : ISODate("2001-04-01T05:00:00Z")
+//           }
+//         },
+//         "inputStage" : {
+//           "stage" : "IXSCAN",
+//           "keyPattern" : {
+//             "headers.From" : 1
+//           },
+//           "indexName" : "headers.From_1",
+//           "isMultiKey" : false,
+//           "direction" : "forward",
+//           "indexBounds" : {
+//             "headers.From" : [
+//               "[MinKey, MaxKey]"
+//             ]
+//           }
+//         }
+//       }
+//     },
+//     "rejectedPlans" : [ ]
+//   },
+//   "executionStats" : {
+//     "executionSuccess" : true,
+//     "nReturned" : 83057,
+//     "executionTimeMillis" : 726,
+//     "totalKeysExamined" : 120477,
+//     "totalDocsExamined" : 120477,
+//     "executionStages" : {
+//       "stage" : "PROJECTION",
+//       "nReturned" : 83057,
+//       "executionTimeMillisEstimate" : 690,
+//       "works" : 120478,
+//       "advanced" : 83057,
+//       "needTime" : 37420,
+//       "needFetch" : 0,
+//       "saveState" : 941,
+//       "restoreState" : 941,
+//       "isEOF" : 1,
+//       "invalidates" : 0,
+//       "transformBy" : {
+//         "headers.From" : 1,
+//         "_id" : 0
+//       },
+//       "inputStage" : {
+//         "stage" : "FETCH",
+//         "filter" : {
+//           "headers.Date" : {
+//             "$gt" : ISODate("2001-04-01T05:00:00Z")
+//           }
+//         },
+//         "nReturned" : 83057,
+//         "executionTimeMillisEstimate" : 350,
+//         "works" : 120478,
+//         "advanced" : 83057,
+//         "needTime" : 37420,
+//         "needFetch" : 0,
+//         "saveState" : 941,
+//         "restoreState" : 941,
+//         "isEOF" : 1,
+//         "invalidates" : 0,
+//         "docsExamined" : 120477,
+//         "alreadyHasObj" : 0,
+//         "inputStage" : {
+//           "stage" : "IXSCAN",
+//           "nReturned" : 120477,
+//           "executionTimeMillisEstimate" : 60,
+//           "works" : 120477,
+//           "advanced" : 120477,
+//           "needTime" : 0,
+//           "needFetch" : 0,
+//           "saveState" : 941,
+//           "restoreState" : 941,
+//           "isEOF" : 1,
+//           "invalidates" : 0,
+//           "keyPattern" : {
+//             "headers.From" : 1
+//           },
+//           "indexName" : "headers.From_1",
+//           "isMultiKey" : false,
+//           "direction" : "forward",
+//           "indexBounds" : {
+//             "headers.From" : [
+//               "[MinKey, MaxKey]"
+//             ]
+//           },
+//           "keysExamined" : 120477,
+//           "dupsTested" : 0,
+//           "dupsDropped" : 0,
+//           "seenInvalidated" : 0,
+//           "matchTested" : 0
+//         }
+//       }
+//     }
+//   },
+//   "serverInfo" : {
+//     "host" : "dpercy-mac-air.local",
+//     "port" : 27017,
+//     "version" : "3.0.1",
+//     "gitVersion" : "534b5a3f9d10f00cd27737fbcd951032248b5952"
+//   },
+//   "ok" : 1
+// }
+// Check below all the statements that are true about the way MongoDB handled this query.
+
+// Check all that apply:
+// The query used an index to figure out which documents match the find criteria.
+// The query avoided sorting the documents because it was able to use an index's ordering.
+// The query returned 120,477 documents.
+// The query scanned every document in the collection.
+// Submit
+// Your submission has been saved, and will be graded when the problem closes.
+
+
+// My answer: The query avoided sorting the documents because it was able to use an index's ordering 
+// AND
+// The query scanned every document in the collection.
